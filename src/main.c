3b8b36e57969125f2cc951c7c1c75931ab2e68df
ermiry <erick.salas@ermiry.com>
2021-01-02 17:23:16 -0600

    Added base main source

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+int main (int argc, char const **argv) {
+
+	(void) printf ("ctest is working!\n\n");
+
+	return 0;
+
+}
+
+#pragma GCC diagnostic pop
